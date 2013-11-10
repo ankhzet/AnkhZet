d9ec72b4b50e0afb988e98fa078d121c03f50a1d
AnkhZet <ankhzet@gmail.com>
2013-11-10 22:53:00 +0200

    Author-update-frequency/dependant samlib-check scheduler

--- cron_authors.php
+++ cron_authors.php
@@ -3,18 +3,19 @@
 
 	require_once 'core_history.php';
 	require_once 'core_updates.php';
+	require_once 'core_authors.php';
 
 	$u = new AuthorWorker();
 
 	$h = HistoryAggregator::getInstance();
 	$a = $h->authorsToUpdate(0, uri_frag($_REQUEST, 'force'));
-	if (count($a)) {
+	if (!!$a) {
 		echo "Authors to update: " . (count($a)) . "<br />";
 		foreach ($a as $id)
 			$u->check($id);
 	}
 
-	$g = $h->groupsToUpdate(uri_frag($r, 0));
+	$g = $h->groupsToUpdate(uri_frag($_REQUEST, 0));
 	if (!!$g) {
 		echo "Groups to update: " . (count($g)) . "<br />";
 		foreach ($g as $id)
@@ -24,3 +25,4 @@
 	if (!$a && !$g)
 		echo Loc::lget('nothing_to_update') . '<br />';
 
+	$h->calcCheckFreq();
